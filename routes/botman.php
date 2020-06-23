42e91f264fb63e8e84e0cf48a00495ac978fd91d
Javier <javiermarmolperez@gmail.com>
2020-06-23 12:07:43 +0200

    Javi/explicacion botman

--- routes/botman.php
+++ routes/botman.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+    $bot->reply('Hello! que quieres hacer?' );
+});
+
+$botman->hears('Hola', function ($bot) {
+    $bot->reply('Hola picha!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
